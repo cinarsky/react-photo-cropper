94626fac6ac9823dbf44a25d119536d284558d0a
cinarsky <cinarsky@sina.com>
2020-12-31 00:22:41 +0800

    i

--- src/react-photo-cropper.js
+++ src/react-photo-cropper.js
@@ -45,63 +45,125 @@ class ReactPhotoCropper extends Component {
     // console.log(1,e.layerX, e.layerY);
     console.log(e, e.layerX, e.layerY);
     if (e.target === this.cropperBoxRef) {
+      const { edge } = this.state
+      const cutterOriginX = this.cropperBoxRef.getBoundingClientRect().left
+      const cutterOriginY = this.cropperBoxRef.getBoundingClientRect().top
+      const pointerX = e.clientX
+      const pointerY = e.clientY
+
+      console.log(cutterOriginX, cutterOriginY, pointerX, pointerY, e.x, e.y);
+      const distanceX = pointerX - cutterOriginX
+      const distanceY = pointerY - cutterOriginY
+      //右侧线上
+      if (Math.abs(distanceX - this.cropperBoxRef.offsetWidth) < edge) {
+        console.log(Math.abs(this.cropperBoxRef.offsetHeight - distanceY), edge);
+        if (Math.abs(this.cropperBoxRef.offsetHeight - distanceY) < edge) {
+          console.log('右下角');
+          this.setState({
+            mode: 'right-bottom'
+          })
+        } else if (Math.abs(distanceY) < edge) {
+          console.log('右上角');
+          this.setState({
+            mode: 'right-top'
+          })
+        } else {
+          console.log('右');
+          this.setState({
+            mode: 'right'
+          })
+        }//下侧线
+      } else if (Math.abs(distanceY - this.cropperBoxRef.offsetHeight) < edge) {
+        if (Math.abs(distanceX) < edge) {
+          console.log('左下');
+          this.setState({
+            mode: 'left-bottom'
+          })
+        } else {
+          console.log('下');
+          this.setState({
+            mode: 'bottom'
+          })
+        }//左侧线
+      } else if (Math.abs(distanceX) < edge) {
+        if (Math.abs(distanceY) < edge) {
+          console.log('左上');
+          this.setState({
+            mode: 'left-top'
+          })
+        } else {
+          console.log('左');
+          this.setState({
+            mode: 'left'
+          })
+        }//上侧线
+      } else if (Math.abs(distanceY) < edge) {
+        console.log('上');
+        this.setState({
+          mode: 'top'
+        })
+      }
+
       this.cropperContentRef.addEventListener('mousemove', this.handleContentMouseMove)
       this.cropperContentRef.addEventListener('mouseup', this.handleContentMouseUp)
     }
     e.preventDefault()
   }
-  //裁剪框拖拽逻辑
+  //裁剪框边缘拖拽逻辑
   handleContentMouseMove = (e) => {
-    const { edge } = this.state
-    console.log(e.offsetX, e.offsetY);
+    const { mode } = this.state
+    const cutterOriginX = this.cropperBoxRef.getBoundingClientRect().left
+    const cutterOriginY = this.cropperBoxRef.getBoundingClientRect().top
+    const pointerX = e.clientX
+    const pointerY = e.clientY
+
+    const distanceX = pointerX - cutterOriginX
+    const distanceY = pointerY - cutterOriginY
+    const top = Number((this.cropperBoxRef.style.top || '0px').split('px')[0])
     let left = Number((this.cropperBoxRef.style.left || '0px').split('px')[0])
-    let top = Number((this.cropperBoxRef.style.top || '0px').split('px')[0])
-    let width = e.offsetX 
-    let height = e.offsetY 
-    // if (e.target === this.cropperContentRef) {
-    //   width = e.offsetX - left
-    //   height = e.offsetY - top
-    // }
-    // if (e.layerX > edge && e.layerX < this.cropperBoxRef.offsetWidth - edge && e.layerY < edge) {
-    //   console.log('上');
-    // } else if (e.layerX <= edge && e.layerY <= edge) {
-    //   console.log('左上');
-    // } else if (e.layerX <= edge && e.layerY <= this.cropperBoxRef.offsetHeight - edge) {
-    //   console.log('左');
-    // } else if (e.layerX <= edge && e.layerY > this.cropperBoxRef.offsetHeight - edge) {
-    //   console.log('左下', e.offsetY,);
 
-    //   this.cropperBoxRef.style.width = `${this.cropperBoxRef.offsetWidth - e.offsetX}px`
-    //   this.cropperBoxRef.style.height = `${e.offsetY}px`
-    //   this.cropperBoxRef.style.left = `${left + e.offsetX}px`
 
-    // } 
-    // else 
-    if (e.layerX > edge && e.layerX <= this.cropperBoxRef.offsetWidth - edge && e.layerY > this.cropperBoxRef.offsetHeight / 2) {
-      console.log('下');
-      this.cropperBoxRef.style.height = `${height}px`
-    } else if (e.layerX > this.cropperBoxRef.offsetWidth - edge && e.layerY > this.cropperBoxRef.offsetHeight - edge) {
-      console.log('右下');
-      console.log(e, width, height);
-      this.cropperBoxRef.style.width = `${width}px`
-      this.cropperBoxRef.style.height = `${height}px`
-    } 
-    // else if (e.layerX > this.cropperBoxRef.offsetWidth - edge && e.layerY <= edge) {
-    //   console.log('右上');
-    // }
-     else if (e.layerX > this.cropperBoxRef.offsetWidth / 2 && e.layerY > edge && e.layerY <= this.cropperBoxRef.offsetHeight - edge) {
-      console.log('右');
-      this.cropperBoxRef.style.width = `${width}px`
-    } else {
-      console.log('其他');
+    console.log(distanceX, distanceY, this.cropperBoxRef.style.top);
+    if (mode === 'right-bottom') {
+      this.cropperBoxRef.style.width = `${distanceX}px`
+      this.cropperBoxRef.style.height = `${distanceY}px`
+    } else if (mode === 'right') {
+      this.cropperBoxRef.style.width = `${distanceX}px`
+    } else if (mode === 'right-top') {
+      this.cropperBoxRef.style.top = `${top + distanceY}px`
+      this.cropperBoxRef.style.width = `${distanceX}px`
+      this.cropperBoxRef.style.height = `${this.cropperBoxRef.offsetHeight - distanceY}px`
+    } else if (mode === 'bottom') {
+      this.cropperBoxRef.style.height = `${distanceY}px`
+    } else if (mode === 'left-bottom') {
+      this.cropperBoxRef.style.left = `${left + distanceX}px`
+      this.cropperBoxRef.style.width = `${this.cropperBoxRef.offsetWidth - distanceX}px`
+      this.cropperBoxRef.style.height = `${distanceY}px`
+    } else if (mode === 'left') {
+      this.cropperBoxRef.style.left = `${left + distanceX}px`
+      this.cropperBoxRef.style.width = `${this.cropperBoxRef.offsetWidth - distanceX}px`
+    } else if (mode === 'left-top') {
+      this.cropperBoxRef.style.top = `${top + distanceY}px`
+      this.cropperBoxRef.style.left = `${left + distanceX}px`
+      this.cropperBoxRef.style.width = `${this.cropperBoxRef.offsetWidth - distanceX}px`
+      this.cropperBoxRef.style.height = `${this.cropperBoxRef.offsetHeight - distanceY}px`
+    } else if (mode === 'top') {
+      this.cropperBoxRef.style.top = `${top + distanceY}px`
+      this.cropperBoxRef.style.height = `${this.cropperBoxRef.offsetHeight - distanceY}px`
     }
+
+
     // e.stopPropagation()
     e.preventDefault()
   }
   handleContentMouseUp = (e) => {
     this.cropperContentRef.removeEventListener('mousemove', this.handleContentMouseMove)
     this.cropperContentRef.removeEventListener('mouseup', this.handleContentMouseUp)
+    this.setState({
+      mode: ''
+    })
   }
+  //裁剪框开始内部拖拽逻辑
   handleMouseDown = (e) => {
     if (e.target === this.cropperBoxRef) {
       return
@@ -114,6 +176,7 @@ class ReactPhotoCropper extends Component {
     this.cropperBoxContentRef.addEventListener('mouseup', this.handleMouseUp)
     e.preventDefault()
   }
+  //裁剪框内部拖拽逻辑
   handleMouseMove = (e) => {
     const { pointerX, pointerY } = this.state
     let left = Number((this.cropperBoxRef.style.left || '0px').split('px')[0])
@@ -128,18 +191,30 @@ class ReactPhotoCropper extends Component {
     this.cropperBoxRef.style.top = `${top}px`
     e.preventDefault()
   }
+  //裁剪框结束内部拖拽逻辑
   handleMouseUp = (e) => {
     this.cropperBoxContentRef.removeEventListener('mousemove', this.handleMouseMove)
     this.cropperBoxContentRef.removeEventListener('mouseup', this.handleMouseUp)
   }
+
   closeDialog = () => {
     this.cropperContentRef.removeEventListener('mousedown', this.handleContentMouseDown)
     // this.cropperImgRef.removeEventListener('mousemove', this.handleImgMouseMove)
     this.cropperBoxContentRef.removeEventListener('mousedown', this.handleMouseDown)
+    const originX = this.cropperBoxContentRef.getBoundingClientRect().left - this.cropperContentRef.getBoundingClientRect().left
+    const originY = this.cropperBoxContentRef.getBoundingClientRect().top - this.cropperContentRef.getBoundingClientRect().top
+    
+
+    console.log('左上',originX,originY);
+    console.log('右上',originX+this.cropperBoxContentRef.offsetWidth,originY);
+    console.log('右下',originX+this.cropperBoxContentRef.offsetWidth,originY+this.cropperBoxContentRef.offsetHeight);
+    console.log('左下',originX,originY+this.cropperBoxContentRef.offsetHeight);
+
     this.setState({
       visible: false
     })
   }
+
   render() {
     const { resetInput, visible, currentImgUrl } = this.state
     return (
@@ -194,3 +269,47 @@ class ReactPhotoCropper extends Component {
   }
 }
 export default ReactPhotoCropper;
+
+
+// console.log(e.offsetX, e.offsetY);
+// let left = Number((this.cropperBoxRef.style.left || '0px').split('px')[0])
+// let top = Number((this.cropperBoxRef.style.top || '0px').split('px')[0])
+// let width = e.offsetX 
+// let height = e.offsetY 
+// if (e.target === this.cropperContentRef) {
+//   width = e.offsetX - left
+//   height = e.offsetY - top
+// }
+// if (e.layerX > edge && e.layerX < this.cropperBoxRef.offsetWidth - edge && e.layerY < edge) {
+//   console.log('上');
+// } else if (e.layerX <= edge && e.layerY <= edge) {
+//   console.log('左上');
+// } else if (e.layerX <= edge && e.layerY <= this.cropperBoxRef.offsetHeight - edge) {
+//   console.log('左');
+// } else if (e.layerX <= edge && e.layerY > this.cropperBoxRef.offsetHeight - edge) {
+//   console.log('左下', e.offsetY,);
+
+//   this.cropperBoxRef.style.width = `${this.cropperBoxRef.offsetWidth - e.offsetX}px`
+//   this.cropperBoxRef.style.height = `${e.offsetY}px`
+//   this.cropperBoxRef.style.left = `${left + e.offsetX}px`
+
+// } 
+// else 
+// if (e.layerX > edge && e.layerX <= this.cropperBoxRef.offsetWidth - edge && e.layerY > this.cropperBoxRef.offsetHeight / 2) {
+//   console.log('下');
+//   this.cropperBoxRef.style.height = `${height}px`
+// } else if (e.layerX > this.cropperBoxRef.offsetWidth - edge && e.layerY > this.cropperBoxRef.offsetHeight - edge) {
+//   console.log('右下');
+//   console.log(e, width, height);
+//   this.cropperBoxRef.style.width = `${width}px`
+//   this.cropperBoxRef.style.height = `${height}px`
+// } 
+// // else if (e.layerX > this.cropperBoxRef.offsetWidth - edge && e.layerY <= edge) {
+// //   console.log('右上');
+// // }
+//  else if (e.layerX > this.cropperBoxRef.offsetWidth / 2 && e.layerY > edge && e.layerY <= this.cropperBoxRef.offsetHeight - edge) {
+//   console.log('右');
+//   this.cropperBoxRef.style.width = `${width}px`
+// } else {
+//   console.log('其他');
+// }
